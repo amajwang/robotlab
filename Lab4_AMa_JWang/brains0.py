0b2627786e5665646aed179ad1acfe45b48cfd71
amajwang <920873247@qq.com>
2015-02-26 21:32:55 -0800

    b

--- Lab4_AMa_JWang/brains0.py
+++ Lab4_AMa_JWang/brains0.py
@@ -165,10 +165,20 @@ def main():
         elif D.STATE == "MOVING_FORWARD":
             
             forward_speed = 100
-            
+
             left_diff = D.MPW_left[1] if D.MPW_left else 0
             right_diff = D.MPW_right[1] if D.MPW_right else 0
             angle_speed = int(60 * min(left_diff+pi/2, right_diff-pi/2, key = abs))
+
+            distance_threshold = 100
+
+            if D.MPW_left and D.MPW_right:
+                left_dis, right_dis = D.MPW_left[2], D.MPW_right[2]
+                print left_dis, right_dis
+                
+                if (left_dis - right_dis > distance_threshold and angle_speed > 0) \
+                or (right_dis - left_dis > distance_threshold and angle_speed < 0):
+                    angle_speed = 0
 
             print angle_speed
 
