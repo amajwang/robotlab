28a934a8df75f5f53ff4cae35d6d2bf167c1c96a
amajwang <920873247@qq.com>
2015-03-12 00:42:13 -0700

    c

--- Lab4_AMa_JWang/brains0.py
+++ Lab4_AMa_JWang/brains0.py
@@ -162,7 +162,7 @@ def main():
 
         elif D.STATE == "MOVING_FORWARD":
             
-            if D.MPW_front and D.MPW_front[3] > 400 and abs(D.MPW_front[1]) < pi/8 and D.MPW_front[2] < 300:
+            if D.MPW_front and D.MPW_front[3] > 200 and abs(D.MPW_front[1]) < pi/8 and D.MPW_front[2] < 300:
                 D.STATE = "TURN_LEFT"
 
 
